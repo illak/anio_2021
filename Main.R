a478dc35f5516094e09d442817995920dfd377dd
Illak <i.zapata1989@gmail.com>
2021-12-06 15:57:20 -0300

    Initial commit

---
+++ Main.R
@@ -0,0 +1,185 @@
+library(tidyverse)
+library(ggtext)
+library(patchwork)
+
+
+data_prop <- read_csv("data/data_propuestas.csv")
+
+# Corregimos tipo de carrera
+data_prop <- data_prop %>% 
+  mutate(tipo = case_when(
+    tipo=="Trayecto"~"Postítulo",
+    tipo=="Profesorado"~"Profesorado",
+    tipo=="Seminario"~"Curso",
+    TRUE ~ "Formación\nacadémica"
+  ))
+
+# Por línea formativa
+data_lf <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES")) %>% 
+  group_by(linea_formativa) %>% 
+  summarise(cursantes = sum(cursantes_T3),
+            propuestas = n_distinct(propuesta))
+
+# Por propuesta 2
+propuesta <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES")) %>%
+  group_by(propuesta) %>% 
+  summarise(cursantes = sum(cursantes_T3))
+
+data_lf_tipo <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES")) %>% 
+  group_by(linea_formativa, tipo) %>% 
+  summarise(cursantes = sum(cursantes_T3),
+            propuestas = n_distinct(propuesta)) %>% 
+  mutate(cursantes_lf = sum(cursantes),
+         propuestas_lf = sum(propuestas))
+
+
+color_lf <- "#5A45A3"
+color_tipo <- "#5A45A3"
+
+plot_resumen_lf <- ggplot(data_lf, aes(x = fct_reorder(linea_formativa,cursantes), y = cursantes)) +
+  geom_point(aes(size=propuestas), color="red") +
+  geom_text(aes(label=cursantes), vjust= -1.8, color="blue", fontface="bold", size=4) +
+  geom_text(aes(label=propuestas), color="white", fontface="bold") +
+  coord_flip(clip="off") +
+  scale_size(range = c(6,13)) +
+  labs(y=NULL, x = NULL,
+       title = "Cursantes ISEP 2021",
+       subtitle = "Cantidad de <span style='color:red;'>propuestas</span> y cantidad de 
+       <span style='color:blue;'>cursantes activos</span> del año por línea formativa y tipo de propuesta") +
+  guides(size = "none") +
+  theme_minimal(base_size = 16) +
+  theme(
+    axis.text.x = element_blank(),
+    plot.title = element_markdown(),
+    plot.subtitle = element_markdown(),
+    plot.title.position = "plot",
+    axis.text.y = element_text(face="bold", color=color_lf),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+#plot_resumen_lf
+
+flechas <- data.frame(x1=c(1.8,1.8), 
+                      x2=c(1,1.2), 
+                      y1=c(2,2.9), 
+                      y2=c(2.9,2.9))
+
+texto <- data.frame(x1=c(1.8), 
+                    x2=c(1.2), 
+                    y1=c(3), 
+                    y2=c(3.1))
+
+
+# SIMILAR AL PLOT ANTERIOR PERO SE AGREGA LA DIM DE TIPO DE PROPUESTA
+plot_resumen_lf_tipo <- ggplot(data_lf_tipo, aes(x = fct_reorder(linea_formativa,cursantes_lf), y = tipo)) +
+  geom_point(aes(size=propuestas), color="red") +
+  geom_text(aes(label=cursantes), vjust= -1.4, color="blue", fontface="bold") +
+  geom_text(aes(label=propuestas), color="white", fontface="bold") +
+# geom_curve(
+#     aes(x=x1,y=y1,xend=x2,yend=y2),
+#     data=flechas,
+#     arrow=arrow(length=unit(0.03, "npc")),
+#     curvature = .3
+#   ) +
+#    annotate(
+#      "text", x=1, y=3, 
+#      label="Acompañamiento a las Trayectorias Escolares en la Educación Secundaria\nTrayectorias Escolares y Enseñanza en el PIT
+# ", hjust=0
+#    ) +
+  coord_flip(clip="off") +
+  scale_size(range = c(6,10)) +
+  scale_y_discrete(position="right") +
+  guides(size = "none") +
+  theme_minimal(base_size = 16) +
+  theme(
+    
+    axis.text.y = element_blank(),
+    axis.title = element_blank(),
+    axis.line.y = element_line(color="grey60"),
+    axis.text.x = element_text(face="bold", color = color_tipo),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+plot_resumen_lf_tipo
+
+
+plot_final_lf_tipo <- plot_resumen_lf + plot_resumen_lf_tipo
+
+ggsave("plot_final_lf_tipo.png", width = 17, height = 10, dpi=320)
+
+
+# PLOT por IFDA
+data_ifda <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES")) %>% 
+  group_by(ifda_sigla) %>% 
+  summarise(cursantes = sum(cursantes_T3),
+            propuestas = n_distinct(propuesta))
+
+
+data_ifda_tipo <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES")) %>% 
+  group_by(ifda_sigla, tipo) %>% 
+  summarise(cursantes = sum(cursantes_T3),
+            propuestas = n_distinct(propuesta)) %>% 
+  mutate(cursantes_ifda = sum(cursantes),
+         propuestas_ifda = sum(propuestas))
+
+
+# PLOT IFDA
+color_lf <- "#5A45A3"
+color_tipo <- "#5A45A3"
+
+plot_resumen_ifda <- ggplot(data_ifda, aes(x = fct_reorder(ifda_sigla,cursantes), y = cursantes)) +
+  geom_point(aes(size=propuestas), color="red") +
+  geom_text(aes(label=cursantes), vjust= -1.8, color="blue", fontface="bold", size=4) +
+  geom_text(aes(label=propuestas), color="white", fontface="bold") +
+  coord_flip(clip="off") +
+  scale_size(range = c(6,13)) +
+  labs(y=NULL, x = NULL,
+       title = "Cursantes ISEP 2021",
+       subtitle = "Cantidad de <span style='color:red;'>propuestas</span> y cantidad de 
+       <span style='color:blue;'>cursantes activos</span> del año por IFDA y tipo de propuesta") +
+  guides(size = "none") +
+  theme_minimal(base_size = 16) +
+  theme(
+    axis.text.x = element_blank(),
+    plot.title = element_markdown(),
+    plot.subtitle = element_markdown(),
+    plot.title.position = "plot",
+    axis.text.y = element_text(face="bold", color=color_lf),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+# PLOT IFDA TIPO
+plot_resumen_ifda_tipo <- ggplot(data_ifda_tipo, aes(x = fct_reorder(ifda_sigla,cursantes_ifda), y = tipo)) +
+  geom_point(aes(size=propuestas), color="red") +
+  geom_text(aes(label=cursantes), vjust= -1.4, color="blue", fontface="bold") +
+  geom_text(aes(label=propuestas), color="white", fontface="bold") +
+coord_flip(clip="off") +
+  scale_size(range = c(6,10)) +
+  scale_y_discrete(position="right") +
+  guides(size = "none") +
+  theme_minimal(base_size = 16) +
+  theme(
+    
+    axis.text.y = element_blank(),
+    axis.title = element_blank(),
+    axis.line.y = element_line(color="grey60"),
+    axis.text.x = element_text(face="bold", color = color_tipo),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+plot_final_ifda_tipo <- plot_resumen_ifda + plot_resumen_ifda_tipo
+ggsave("plot_final_ifda_tipo.png", plot_final_ifda_tipo, width = 17, height = 10, dpi=320)
