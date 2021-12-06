91fce1c236f4f90f864b1e2718364adf548471e6
Illak <i.zapata1989@gmail.com>
2021-12-06 17:06:31 -0300

    update

--- Main.R
+++ Main.R
@@ -183,3 +183,50 @@ coord_flip(clip="off") +
 
 plot_final_ifda_tipo <- plot_resumen_ifda + plot_resumen_ifda_tipo
 ggsave("plot_final_ifda_tipo.png", plot_final_ifda_tipo, width = 17, height = 10, dpi=320)
+
+# Pruebas VIZ crecimiento 
+data_anios <- read_csv("data/data_anios.csv") %>% 
+  mutate(tipo = case_when(
+    tipo=="Trayecto"~"Postítulo",
+    tipo=="Profesorado"~"Profesorado",
+    tipo=="Seminario"~"Curso",
+    TRUE ~ "Formación\nacadémica"
+  )) %>% 
+  mutate(anio=as.factor(anio),
+         anio=factor(anio, levels=c("2016","2017","2018","2019","2020","2021"))) %>% 
+  filter(anio %in% c(2016,2019,2020,2021)) %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(!carrera_siglas %in% c("CLINICAESCRITURA","MOD_INDIVIDUALES","TEST_TRAYECTO"))
+
+data_anios <- data_anios %>% 
+  group_by(anio) %>% 
+  summarise(cursantes = sum(cursantes_T3),
+            propuestas_vigentes = n_distinct(propuesta))
+
+
+plot_evol <- ggplot(data_anios, aes(x=anio, y=cursantes, group=1)) +
+  annotate("rect", xmin=1,xmax=4, ymin=-Inf, ymax=Inf, alpha=.2, fill="grey60") +
+  annotate("rect", xmin=4,xmax=6, ymin=-Inf, ymax=Inf, alpha=.2, fill="lightgreen") +
+  geom_line(linetype="dashed") +
+  geom_point(aes(size=cursantes)) +
+  geom_label(aes(label=cursantes), vjust=-1) +
+  scale_x_discrete(drop=FALSE, labels=c("2016","","","2019","2020","2021")) +
+  coord_cartesian(clip="off") +
+  scale_size(range = c(4,12)) +
+  guides(size="none") +
+  labs(x=NULL,y=NULL,
+       title="Crecimiento en la cantidad de cursantes desde el inicio hasta los últimos 3 años") +
+  theme_minimal(base_size = 14) +
+  theme(
+    panel.grid.minor.y = element_blank(),
+    axis.text.x = element_text(face="bold"),
+    plot.title = element_text(margin = margin(0,0,20,0)),
+    plot.title.position = "plot",
+    plot.background = element_rect(fill="white")
+  )
+
+plot_evol
+
+
+ggsave("plot_evol.png", plot_evol, width = 10, height = 10, dpi=320)
+
