88910bb0a1aa5ad31f6906d17b1278802021df8d
Illak <i.zapata1989@gmail.com>
2021-12-14 10:16:55 -0300

    update

--- Main.R
+++ Main.R
@@ -3,7 +3,7 @@ library(ggtext)
 library(patchwork)
 
 
-data_prop <- read_csv("data/data_propuestas.csv")
+data_prop <- read_csv("data/data_propuestas_2.csv")
 
 # Corregimos tipo de carrera
 data_prop <- data_prop %>% 
@@ -51,7 +51,7 @@ plot_resumen_lf <- ggplot(data_lf, aes(x = fct_reorder(linea_formativa,cursantes
   geom_text(aes(label=propuestas, size=4*(propuestas*.03)), color="white", fontface="bold") +
   coord_flip(clip="off") +
   scale_size(range = c(6,15)) +
-  labs(y="<-- Cantidad de cursantes -->", x = NULL,
+  labs(y=NULL, x = NULL,
        #title = "Cursantes ISEP 2021",
        #subtitle = "Cantidad de <span style='color:red;'>propuestas</span> y cantidad de 
        #<span style='color:blue;'>cursantes activos</span> del año por línea formativa y tipo de propuesta"
@@ -85,32 +85,25 @@ texto <- data.frame(x1=c(1.8),
 # SIMILAR AL PLOT ANTERIOR PERO SE AGREGA LA DIM DE TIPO DE PROPUESTA
 plot_resumen_lf_tipo <- ggplot(data_lf_tipo, aes(x = fct_reorder(linea_formativa,cursantes_lf), y = tipo)) +
   geom_point(aes(size=propuestas), color=color_2) +
-  geom_text(aes(label=cursantes), vjust= -1.4, color=color_1, fontface="bold", size=6) +
+  geom_text(aes(label=cursantes), vjust= -1.4, color=color_1, fontface="bold", size=7) +
   geom_text(aes(label=propuestas, size=4*(propuestas*.03)), color="white", fontface="bold") +
-# geom_curve(
-#     aes(x=x1,y=y1,xend=x2,yend=y2),
-#     data=flechas,
-#     arrow=arrow(length=unit(0.03, "npc")),
-#     curvature = .3
-#   ) +
-#    annotate(
-#      "text", x=1, y=3, 
-#      label="Acompañamiento a las Trayectorias Escolares en la Educación Secundaria\nTrayectorias Escolares y Enseñanza en el PIT
-# ", hjust=0
-#    ) +
   coord_flip(clip="off") +
   scale_size(range = c(6,13)) +
   scale_y_discrete(position="right") +
+  #expand_limits(x=c(1,14)) +
   guides(size = "none") +
-  theme_minimal(base_size = 18) +
+  theme_minimal() +
   theme(
-    
+    axis.text = element_text(size=20),
+    panel.background = element_rect(fill="#F2F2F2", color=NA),
     axis.text.y = element_blank(),
     axis.title = element_blank(),
-    axis.line.y = element_line(color="grey60"),
-    axis.text.x = element_text(face="bold", color = color_tipo, angle = 45, hjust=0),
+    #axis.line.y = element_line(color="grey60"),
+    axis.text.x = element_text(face="bold", color = color_tipo),
+    axis.text.x.top = element_text(vjust=.5),
     panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_line(color="grey80")
   )
 
 plot_resumen_lf_tipo
@@ -119,7 +112,7 @@ plot_resumen_lf_tipo
 plot_final_lf_tipo <- plot_resumen_lf + plot_resumen_lf_tipo
 
 plot_final_lf_tipo
-ggsave("plot_final_lf_tipo.png", width = 19, height = 10, dpi=320)
+ggsave("plot_final_lf_tipo.png", width = 23, height = 10, dpi=320)
 
 
 # PLOT por IFDA
@@ -171,6 +164,7 @@ plot_resumen_ifda <- ggplot(data_ifda, aes(x = fct_reorder(ifda_sigla,cursantes)
     panel.grid.minor.x = element_blank()
   )
 
+
 # PLOT IFDA TIPO
 plot_resumen_ifda_tipo <- ggplot(data_ifda_tipo, aes(x = fct_reorder(ifda_sigla,cursantes_ifda), y = tipo)) +
   geom_point(aes(size=propuestas), color=color_2) +
@@ -194,7 +188,7 @@ coord_flip(clip="off") +
     panel.grid.minor.x = element_blank(),
     panel.grid.major.y = element_line(color="grey80")
   )
-
+plot_resumen_ifda_tipo
 plot_final_ifda_tipo <- plot_resumen_ifda + plot_resumen_ifda_tipo
 plot_final_ifda_tipo
 ggsave("plot_final_ifda_tipo.png", plot_final_ifda_tipo, width = 20, height = 13, dpi=320)
@@ -220,8 +214,8 @@ data_anios <- data_anios %>%
 
 
 plot_evol <- ggplot(data_anios, aes(x=anio, y=cursantes, group=1)) +
-  annotate("rect", xmin=1,xmax=4, ymin=-Inf, ymax=Inf, alpha=.2, fill="grey60") +
-  annotate("rect", xmin=4,xmax=6, ymin=-Inf, ymax=Inf, alpha=.2, fill="lightgreen") +
+  #annotate("rect", xmin=1,xmax=4, ymin=-Inf, ymax=Inf, alpha=.2, fill="grey60") +
+  #annotate("rect", xmin=4,xmax=6, ymin=-Inf, ymax=Inf, alpha=.2, fill="lightgreen") +
   geom_line(linetype="dashed") +
   geom_point(aes(size=cursantes)) +
   geom_label(aes(label=cursantes, hjust=ifelse(cursantes<7000, -.5, 1.5))) +
@@ -230,28 +224,29 @@ plot_evol <- ggplot(data_anios, aes(x=anio, y=cursantes, group=1)) +
   scale_size(range = c(4,12)) +
   guides(size="none") +
   labs(x=NULL,y=NULL,
-       title="Crecimiento en la cantidad de cursantes desde el inicio hasta los últimos 3 años") +
+       #title="Crecimiento en la cantidad de cursantes desde el inicio hasta los últimos 3 años"
+       ) +
   theme_minimal(base_size = 14) +
   theme(
     panel.grid.minor.y = element_blank(),
     axis.text.x = element_text(face="bold"),
     plot.title = element_text(margin = margin(0,0,20,0)),
     plot.title.position = "plot",
-    plot.background = element_rect(fill="white")
+    
   )
 
 plot_evol
 
 
-ggsave("plot_evol.png", plot_evol, width = 10, height = 10, dpi=320)
+ggsave("plot_evol.png", plot_evol, width = 13, height = 5, dpi=320)
 
 
 # Egresados
-data_egresados <- read_csv("data/data_matricula_egresos.csv")
-
+#data_egresados <- read_csv("data/data_matricula_egresos.csv")
+data_egresados <- read_csv("data/data_egresos_2.csv")
 
 data_egresados <- data_egresados %>% 
-  select(linea_formativa, propuesta, carrera_siglas, tipo, ifda_siglas, egresados_intermedio, egresados, anio) %>% 
+  select(linea_formativa, propuesta, carrera_siglas, tipo, egresados, anio) %>% 
   mutate(tipo = case_when(
     tipo=="Trayecto"~"Postítulo",
     tipo=="Profesorado"~"Profesorado",
@@ -263,11 +258,10 @@ data_egresados <- data_egresados %>%
 data_egresados_tipo <- data_egresados %>% 
   mutate(
     anio_actual = as.factor(anio==2021),
-    anio_actual = factor(anio_actual, levels=c("TRUE", "FALSE")),
-    egresados_totales = egresados + egresados_intermedio
+    anio_actual = factor(anio_actual, levels=c("TRUE", "FALSE"))
   ) %>% 
   group_by(tipo, anio_actual) %>% 
-  summarise(egresados_tipo = sum(egresados_totales))
+  summarise(egresados_tipo = sum(egresados))
   
   
 plot_egresados_1 <- ggplot(data_egresados_tipo, aes(x=tipo, y=egresados_tipo, fill=anio_actual)) +
@@ -283,9 +277,37 @@ plot_egresados_1 <- ggplot(data_egresados_tipo, aes(x=tipo, y=egresados_tipo, fi
     fill="none"
   ) +
   theme(
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    axis.text.y = element_text(size=26)
   )
 
 plot_egresados_1
 
 ggsave("plot_egresados.png", width = 10, height = 10, dpi=320)
+
+
+
+
+
+
+
+data_cursantes <- data_prop %>% 
+  filter(cursantes_T3 > 0) %>% 
+  filter(carrera_siglas %in% c(
+    "GESTIONINI_ACT",
+    "GESTIONPRI_ACT",
+    "GESTIONSEC_ACT",
+    "GESTIONSUP_ACT",
+    "CONTINUIDADPEDAGOGICA",
+    "SEMINARIOSECUNDARIOJYA",
+    "INICIAL_ACT",
+    "ATENEONATURALES1",
+    "SEMINARIOINTROPC",
+    "SEMINARIODATIEXP",
+    "SEMINARIOREDDIGIYSOC",
+    "SEMINARIOESI",
+    "ATENEOLENGUA1",
+    "TALLERLAB"
+    ) | cohorte_anio==2021) %>%
+  group_by(propuesta) %>% 
+  summarise(T3 = sum(cursantes_T3))
